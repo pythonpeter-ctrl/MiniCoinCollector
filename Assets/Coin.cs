7853d39d1f4fb923afc052e2959ea9741428adb9
Peti <bodaipeter77@gmail.com>
2026-08-17 16:43:35 +0200

    Jatek 1 proba kesz

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -3,14 +3,34 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     [SerializeField] private GameManager gameManager;
+    [SerializeField] private BoxCollider2D spawnArea;
+    [SerializeField] private Transform player;
+    [SerializeField] private float minimumDistance = 2f;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
         if (other.CompareTag("Player"))
         {
             gameManager.AddScore();
-            Destroy(gameObject);
+            MoveToRandomPosition();
         }
-        
+    }
+
+    private void MoveToRandomPosition()
+    {
+        Bounds bounds = spawnArea.bounds;
+
+        Vector3 newPosition;
+
+        do
+        {
+            float randomX = Random.Range(bounds.min.x, bounds.max.x);
+            float randomY = Random.Range(bounds.min.y, bounds.max.y);
+
+            newPosition = new Vector3(randomX, randomY, 0f);
+        }
+        while (Vector3.Distance(newPosition, player.position) < minimumDistance);
+
+        transform.position = newPosition;
     }
 }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,12 +1,65 @@
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private GameObject joystick;
+    [SerializeField] private TMP_Text timerText;
+    [SerializeField] private TMP_Text finalTimeText;
+
+
+    private float elapsedTime;
     private int score = 0;
+    private const int targetScore = 3;
+
+    private void Awake()
+    {
+        Time.timeScale = 1f;
+
+        timerText.gameObject.SetActive(true);
+        scoreText.text = "SCORE: 0";
+        gameOverPanel.SetActive(false);
+
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        timerText.text = "Time: "+ elapsedTime.ToString("F1")+"s";
+    }
 
     public void AddScore()
     {
         score++;
-        Debug.Log("Score: " +  score);
+
+        scoreText.text = "SCORE: " + score;
+
+        if (score >= targetScore)
+        {
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        gameOverPanel.SetActive(true);
+        joystick.SetActive(false);
+        timerText.gameObject.SetActive(false);
+        finalTimeText.text = "Time: " + elapsedTime.ToString("F1") + "s";
+        Time.timeScale = 0f;
+
+        Debug.Log("Game Over! 30 coins collected.");
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(
+            UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex
+        );
     }
 }
